fc01187c830d04aac4649bbe9351ce3e46e96605
jhyunwoo <jhyunwoo0228@gmail.com>
2024-04-13 18:12:44 +0900

    fix lab 6 p1

--- lab6_2024148005/lab6_p1.py
+++ lab6_2024148005/lab6_p1.py
@@ -5,25 +5,9 @@
 
 # Take user's name
 name = input("Enter a first and last name: ")
-# Variable that store first name
-first_name = ""
-# Variable that store last name
-last_name = ""
-# Variable that store name data
-name_data = ""
-
-# Find first and last name in user input using for loop
-for letter in name:
-    if letter.isalpha():  # If letter is alphabet, add letter to name_data
-        name_data += letter
-    elif letter.isspace() and name_data != "" and first_name == "":  # After find all firstname,
-        first_name = name_data  # Assign into first_name variable
-        name_data = ""  # Reset name_data variable for find last name
-
-last_name = name_data
 
 # Split user's name to first name and last name using split method
-name_list = name.split(" ")
+name = name.split()
 
 # Print result
-print(f"{last_name}, {first_name[0]}.")
+print(f"{name[1]}, {name[0][0]}.")
